c76176d0a8b8ccb9b4e15ce0f6ae8fd4ac12599a
Jules Graus <jules.graus@gmail.com>
2019-01-31 23:35:07 +0100

    Basic setup and project structure.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Forest Guide
+
+Guiding your users trough your web app
+
+## Getting Started
+
+More info soon
+
+### Installation
+
+You can install Forest guide via Yarn:
+
+```
+yarn add forestguide --ignore-engines
+```
+### Integration
+
+Just include *node_modules/forestguide/dist/forestguide.js* into your project.
+More information about integration coming soon since the project just started!
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+
+    entry: './src/forestguide.js',
+    output: {
+        filename: 'forestguide.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
